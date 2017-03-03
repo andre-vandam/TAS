176aa2751c367162527304d01dfe34957d6c6902
Jones1311 <geoffreygarrett1311@gmail.com>
2017-03-03 11:21:09 +0100

    Renamed file

--- Geoffrey-scripts/processing.py
+++ Geoffrey-scripts/processing.py
@@ -3,8 +3,10 @@ from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 from math import sqrt
 
+filename = "Gill Log [WM1]-4"
+
 # Data array creation from data.txt
-data = np.genfromtxt('Gill Log [WM1]-4', delimiter =',', skip_header=5, skip_footer=1, dtype=str)
+data = np.genfromtxt(filename, delimiter =',', skip_header=5, skip_footer=1, dtype=str)
 
 # Time stamp string sectioning (removal of date)
 for i in range(len(data[:,10])):
@@ -24,6 +26,7 @@ def timedifference(t1, t2):
     Dt = dh*60*60 + dm*60 + ds #[s]
     return Dt
 
+# Lambda Function to calculate magnitude of vector from components.
 g = lambda x,y,z: sqrt(float(x)**2+float(y)**2+float(z)**2)
 
 t = PrettyTable(['I.D.',"Vx", "Vy", "Vz", "Time", "S.O.S."])
@@ -33,28 +36,28 @@ time = []
 Vlist =[]
 
 for i in range(len(data[:,1])):
-    time.append(timedifference(data[i,10], data[0,10]))
+    time.append(timedifference(data[0,10], data[i,10]))
 
-
-
+# Conversion of velocity components into float, and ignoring of empty cells.
 for j in range(3):
     for i in range(len(data[:,1+j])):
         if data[i,1+j] == '':
             data[i,1+j] = 0
 
+        # Should the cell be empty, the value 0 will be assigned
         elif data[i,1+j] != '':
             data[i,1+j] = float(eval(data[i,1]))
-            print("i: ",i)
 
-
+# Rows added for display in the pretty table
 for i in range(len(data[:,1])):
     t.add_row([i, data[i,1], data[i,2], data[i,3], data[i,10], data[i,5]])
 
+# Calculation of the wind vector through u,v,w components.
 for i in range(len(data[:,1])):
     V = g(data[i,1], data[i,2], data[i,3])
     Vlist.append(V)
 
-# plt.plot(Vlist, time)
-# plt.show()
+plt.plot(time, Vlist)
+plt.show()
 print(t)
 
