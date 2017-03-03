130721a63d0b24198b76b3447e9ee288536db778
andre-vandam <andre.vandam@gmail.com>
2017-03-03 09:52:06 +0100

    lalaland

--- Reformatting.py
+++ Reformatting.py
@@ -4,24 +4,25 @@ Created on Sat Feb 25 22:36:58 2017
 
 @author: Sven Geboers
 """
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
-data = numpy.genfromtxt('Data/1',delimiter = ',')
+convert = lambda time: float(time[(len(time)-7):])
+data = np.genfromtxt('Data/1',delimiter = ',', dtype =str, converters ={10:convert})
 length = len(data)
 data = data[:]
+print(data)
+# ## Velocity components ##
+# U = data[:,1]
+# V = data[:,2]
+# W = data[:,3]
+# ##=====================##
+# SOS = data[:,5] #Speed of Sound
+# OtherData = data[:,6]   # not sure what it is
+# index = data[:,9] # Index number of the data
+# timestamp = data[:,10]
 
-## Velocity components ##
-U = data[:,1]
-V = data[:,2]
-W = data[:,3]
-##=====================##
-SOS = data[:,5] #Speed of Sound
-OtherData = data[:,6]   # not sure what it is
-index = data[:,9] # Index number of the data
-timestamp = data[:,10]
 
-
-plt.scatter(index,SOS)
-plt.show()
-print (timestamp)
+# plt.scatter(index,SOS)
+# plt.show()
+# print (timestamp)
