01bebfc16e9b57536538c8a6f7782c38e50929e4
andre-vandam <andre.vandam@gmail.com>
2017-03-13 16:12:27 +0100

    minor changes to statistics

--- StatisticalAnal/Statistics.py
+++ StatisticalAnal/Statistics.py
@@ -1,8 +1,20 @@
 import sys
 sys.path.append ("../clean_scripts/")
 import data_handler as dp
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 dataFrames = []
 
-for i in range (5,11,1):
-    dataFrames.append(dp.DataFrame("../Data/"+str(i)+".csv"))
+# for i in range (5,11,1):
+#     dataFrames.append(dp.DataFrame("../Data/"+str(i)+".csv"))
+
+test1 = dp.DataFrame("../Data/5.tas.csv")
+
+
+plt.plot(range(np.size(test1.df.Uxy)), test1.df.Uxy )
+plt.plot(range(np.size(test1.df.Uxy)), pd.rolling_mean(test1.df.Uxy,3000), c='r' )
+# plt.plot(range(np.size(test1.df.Theta)), test1.df.Theta )
+
+plt.show()
